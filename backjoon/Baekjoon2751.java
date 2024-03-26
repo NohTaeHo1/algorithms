b51d37ae77bf18865f5dade28625d7fcc194f721
NohTaeHo1 <160007694+NohTaeHo1@users.noreply.github.com>
2024-03-26 19:45:52 +0900

    240326

---
+++ backjoon/Baekjoon2751.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon2751 {
+   /* public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        int[] input2 = Integer.parseInt(input.split(" "));
+
+        for(int i=0;i< input.length;i++){
+            input[i] = sc.nextInt();
+        }
+
+        Arrays.sort(input);
+
+        Arrays.stream(input).forEach(System.out::println);
+    }*/
+}
