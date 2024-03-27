33688f581670122314959cc55be753defcf6e64e
NohTaeHo1 <160007694+NohTaeHo1@users.noreply.github.com>
2024-03-27 21:12:52 +0900

    240327

---
+++ backjoon/Baekjoon10814.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.util.*;
+
+public class Baekjoon10814 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+        String[] input = new String[testCase];
+
+        for(int i=0;i<input.length;i++){
+            input[i] = sc.nextLine();
+        }
+
+        Arrays.sort(input);
+
+        Arrays.stream(input).forEach((i)->{
+            System.out.println(i);
+        });
+    }
+}
--- backjoon/Baekjoon2751.java
+++ backjoon/Baekjoon2751.java
@@ -1,22 +1,28 @@
 package backjoon;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baekjoon2751 {
-   /* public static void main(String[] args) {
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.nextLine();
+       System.out.println();
+       br.write()
+       br.read();
+       bw.flush();
 
-        int[] input2 = Integer.parseInt(input.split(" "));
-
-        for(int i=0;i< input.length;i++){
-            input[i] = sc.nextInt();
-        }
-
-        Arrays.sort(input);
-
-        Arrays.stream(input).forEach(System.out::println);
-    }*/
-}
+//        int[] input2 = Integer.parseInt(input.split(" "));
+//
+//        for(int i=0;i< input.length;i++){
+//            input[i] = sc.nextInt();
+//        }
+//
+//        Arrays.sort(input);
+//
+//        Arrays.stream(input).forEach(System.out::println);
+//    }
+}}
